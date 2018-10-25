65c3981139cf53d28f57ecfd98729ec426aa88fc
konstantin <galperin.15@yandex.ru>
2018-10-25 20:24:15 +0300

    step2

---
+++ src/bin/binary.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import my_ques from '../src/index.js';
+my_ques;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export const my_ques = () =>{
+	const name = readlineSync.question('May I have your name? ');
+	console.log('Hello' + name +'!');
+}
